3b3d706082d953fbe48c5a2cafcaffb8e352b423
Merrin <merrin.macleod@gmail.com>
2015-06-29 17:10:17 +1000

    added in_use column to database, planning how to pick a thing daily

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advice for myself
+
+This is a silly thing that I want to do.
+
+It is at http://advice-for-self.herokuapp.com/
+The database on Heroku is postgres
+To run locally
+`foreman start web`
+To test the update locally
+`foreman start worker`
---
+++ update.js
@@ -0,0 +1,12 @@
+function sayHello() {
+  console.log('Hello');
+}
+sayHello();
+// if there is in_use
+  // select in_use
+  // make it used
+// if there is unused
+  // select random unused
+  // make it in_use
+  // return changed
+// else: return empty
