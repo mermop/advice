d35ba66c0b59d5493cd9ce9a3f24c3b45ddf0f33
Merrin <merrin.macleod@gmail.com>
2015-06-29 15:44:58 +1000

    moving db list to root

--- index.js
+++ index.js
@@ -9,25 +9,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
+app.get('/', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM advice_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('pages/index', {results: result.rows} ); }
     });
   });
 })
