5d2908646b56bc55eaef6a90b7f2bcc3ef7ce6e2
Merrin <merrin.macleod@gmail.com>
2015-06-29 20:16:54 +1000

    trying out the task..

--- update.js
+++ update.js
@@ -1,7 +1,26 @@
-function sayHello() {
-  console.log('Hello');
-}
-sayHello();
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query("UPDATE advice_table SET use = 'used' WHERE use = 'in_use'", function(err, result) {
+    done();
+    if (err)
+     { console.error(err); response.send("Error " + err); }
+    else
+     { 
+      console.log('yep');
+    }
+  });
+});
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query("SELECT advice FROM advice_table ORDER BY RANDOM() LIMIT 1;", function(err, result) {
+    done();
+    if (err)
+     { console.error(err); response.send("Error " + err); }
+    else
+     { 
+      console.log('yep');
+    }
+  });
+});
 // if there is in_use
   // select in_use
   // make it used
