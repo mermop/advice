c558d665fc04825791bbdbaaa7d75735452d3447
Merrin <merrin.macleod@gmail.com>
2015-06-29 17:12:39 +1000

    hopefully this works i should get the database working locally so i don't have to deploy to update every time

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var pg = require('pg');
 
 app.get('/', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM advice_table WHERE use = in_use', function(err, result) {
+    client.query("SELECT * FROM advice_table WHERE use = 'in_use'", function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
