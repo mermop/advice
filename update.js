9ee92ed7bf48aa32d40303cd7dcfa3d83756e31f
Merrin <merrin.macleod@gmail.com>
2015-06-30 11:14:00 +1000

    update script

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 var pg = require('pg');
 
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query("UPDATE advice_table SET use = 'used' WHERE use = 'in_use'", function(err, result) {
+  client.query("UPDATE advice_table SET use = 'used' WHERE use = 'in_use'; UPDATE advice_table SET use='in_use' WHERE id IN (SELECT id FROM advice_table WHERE use = 'unused' ORDER BY RANDOM() LIMIT 1);", function(err, result) {
     done();
     if (err)
      { console.error(err); response.send("Error " + err); }
@@ -11,23 +11,3 @@ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     }
   });
 });
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query("SELECT advice FROM advice_table ORDER BY RANDOM() WHERE use = 'used' LIMIT 1;", function(err, result) {
-    done();
-    if (err)
-     { console.error(err); response.send("Error " + err); }
-    else
-     { 
-      console.log('yep');
-    }
-  });
-});
-// if there is in_use
-  // select in_use
-  // make it used
-// if there is unused
-  // select random unused
-  // make it in_use
-  // return changed
-// else: return empty
