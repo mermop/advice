3cc31d56fc1a746301d58c1eb848ede8853dc30f
Merrin <merrin.macleod@gmail.com>
2015-07-01 13:39:06 +1000

    adding jquery and webshot, making a canvas drawing thing

---
+++ public/script.js
@@ -0,0 +1,71 @@
+$( document ).ready(function() {
+  var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+  // resize the canvas to fill browser window dynamically
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  drawStuff();
+  window.addEventListener('resize', resizeCanvas, false);
+
+  resizeCanvas();
+  function drawStuff() {
+
+    $('#canvas').mousedown(function(e){
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+        
+      paint = true;
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      redraw();
+    });
+
+    $('#canvas').mousemove(function(e){
+      if(paint){
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+        redraw();
+      }
+    });
+
+    $('#canvas').mouseup(function(e){
+      paint = false;
+    });
+
+    $('#canvas').mouseleave(function(e){
+      paint = false;
+    });
+
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+    var paint;
+
+    function addClick(x, y, dragging)
+    {
+      clickX.push(x);
+      clickY.push(y);
+      clickDrag.push(dragging);
+    }
+
+    function redraw(){
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+      
+      context.strokeStyle = "#df4b26";
+      context.lineJoin = "round";
+      context.lineWidth = 3;
+      context.globalAlpha = 0.5;
+          
+      for(var i=0; i < clickX.length; i++) {    
+        context.beginPath();
+        if(clickDrag[i] && i){
+          context.moveTo(clickX[i-1], clickY[i-1]);
+         }else{
+           context.moveTo(clickX[i]-1, clickY[i]);
+         }
+         context.lineTo(clickX[i], clickY[i]);
+         context.closePath();
+         context.stroke();
+      }
+    }
+
+  }
+});
