f23c97e4269f5ffbdeabbcc03bafac66b358dd2b
Roger Nesbitt <roger@seriousorange.com>
2015-07-23 20:44:44 +1200

    Allow users to download their creation!

--- public/script.js
+++ public/script.js
@@ -6,6 +6,32 @@ $( document ).ready(function() {
   canvas.height = window.innerHeight;
   drawStuff();
 
+  $("#download").click(function() {
+    var outputCanvas = document.createElement("canvas");
+    outputCanvas.width = canvas.width;
+    outputCanvas.height = canvas.height;
+
+    var outputContext = outputCanvas.getContext("2d");
+    outputContext.font = "bold 24pt Times";
+
+    var text = $(".advice").text();
+    var textWidth = outputContext.measureText(text).width;
+    var textHeight = 24;
+    var boxWidth = textWidth + 64;
+    var boxHeight = textHeight + 64;
+
+    outputContext.fillStyle = "rgba(255, 255, 255, 0.5)"
+    outputContext.fillRect((canvas.width - boxWidth) / 2, (canvas.height - boxHeight) / 2, boxWidth, boxHeight);
+
+    outputContext.fillStyle = "black";
+    outputContext.fillText(text, (canvas.width - textWidth) / 2, (canvas.height - textHeight) / 2 + textHeight);
+
+    outputContext.drawImage(canvas, 0, 0);
+
+    this.href = outputCanvas.toDataURL();
+    this.download = "advice.png";
+  });
+
   function drawStuff() {
 
     $('#canvas').mousedown(function(e){
